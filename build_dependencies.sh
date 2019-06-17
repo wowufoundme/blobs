490a55e581368994547c8c09f158c76c4d823fab
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2019-06-17 16:54:01 +0530

    Upload build_dependencies.sh
    Build dependencies for Linux for Android ROM building

---
+++ build_dependencies.sh
@@ -0,0 +1,14 @@
+# You can simply run this script with 'sudo' command
+# Enter the password when prompted
+# There might be some packages that may be preinstalled on your system already
+# Don't worry the script will automatically skip those and update
+# This might some time and definitely some internet so keep in mind to have those 
+# things ready.
+#
+# To directly run this script, run the below command:
+# curl -r https:
+# sudo chmod a+x ./build_dependencies.sh sudo ./build_dependencies.sh
+#
+
+# Update the environment
+sudo apt-get update
